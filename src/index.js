86e7c8d726e94eb331b0e9456719c1d2058ce13b
Denis Gusar <degusar.wot@gmail.com>
2021-08-22 13:32:15 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let arrayOfDigits = Array.from(String(n), Number);
+    console.log(arrayOfDigits);
+    if (n > 0) {
+        let reversedInteger = arrayOfDigits.reverse().join('');
+        console.log(reversedInteger);
+        return reversedInteger;
+    } else {
+        arrayOfDigits.shift();
+        let reversedInteger = arrayOfDigits.reverse().join('');
+        console.log(reversedInteger);
+        return reversedInteger;
+    }
+    
 }
